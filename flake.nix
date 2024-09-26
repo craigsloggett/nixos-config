4269c67ccdd180dc138a5e95192a312b973cf57e
Craig Sloggett <craigsloggett@gmail.com>
2024-09-25 21:45:39 -0400

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "NixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.xps = nixpks.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
