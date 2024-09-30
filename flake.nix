bdeb7ac95ae3a86686158aac48beb2695e3446ac
Craig Sloggett <craigsloggett@gmail.com>
2024-09-30 11:25:42 -0400

    chore: add nixos-hardware input

--- flake.nix
+++ flake.nix
@@ -3,14 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
   };
 
   outputs =
-    { self, nixpkgs }@inputs:
+    { self, nixpkgs, nixos-hardware }@inputs:
     {
       nixosConfigurations = {
         xps = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
           modules = [ ./hosts/xps ];
         };
       };
